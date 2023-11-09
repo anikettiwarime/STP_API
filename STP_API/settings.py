4b15dceac3bb58fc3b8a226cee2b0d8d4983d6b7
aniket <anikettiwari1570@gmail.com>
2023-11-09 23:16:45 +0530

    Remove unused import in settings.py

--- STP_API/settings.py
+++ STP_API/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
-import dj_database_url
 from decouple import config
 import cloudinary
 
